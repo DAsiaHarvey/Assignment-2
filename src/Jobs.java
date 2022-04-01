72613080af0a32801af3572ea0a0708a3866d285
D'Asia Harvey <xdrag309@yahoo.com>
2022-04-01 15:36:33 -0500

    first commit

---
+++ src/Jobs.java
@@ -0,0 +1,12 @@
+import net.datastructures.SortedPriorityQueue;
+
+public class Jobs {
+    int priority;
+    int length;
+    int timeWaiting;
+    int maxWaitingTime;
+    String name;
+    SortedPriorityQueue processQ = new SortedPriorityQueue<>();
+
+
+}
